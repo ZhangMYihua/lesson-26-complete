51ac74c463523ec910924835f846ea6b7891477e
Yihua Zhang <michaelzhang2@msn.com>
2019-05-23 19:35:55 +0200

    added clearItemFromCart action and bound it to remove in checkout-item

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -18,6 +18,13 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         ...state,
         cartItems: addItemToCart(state.cartItems, action.payload)
       };
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          cartItem => cartItem.id !== action.payload.id
+        )
+      };
     default:
       return state;
   }
