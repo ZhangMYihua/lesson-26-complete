2b1fdf0df0604cf472e442557b38f75b8f40fe93
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-01-18 22:06:05 +0200

    Update menu-item.styles.jsx

--- src/components/menu-item/menu-item.styles.jsx
+++ src/components/menu-item/menu-item.styles.jsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const MenuItemContainer = styled.div`
-	height: ${({ size }) => (size ? '380px' : '240px')}
+	height: ${({ size }) => (size ? '380px' : '240px')};
 	min-width: 30%;
 	overflow: hidden;
 	flex: 1 1 auto;
