eeae165bba5a2eaf66b7c18252a1c5dcbc857809
Nitin Ranganath <nitinranganath@gmail.com>
2020-07-01 17:22:24 +0530

    Fix hover effect and indentation of menu-item component

--- src/components/menu-item/menu-item.styles.jsx
+++ src/components/menu-item/menu-item.styles.jsx
@@ -1,31 +1,29 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const MenuItemContainer = styled.div`
-	height: ${({ size }) => (size ? '380px' : '240px')};
-	min-width: 30%;
-	overflow: hidden;
-	flex: 1 1 auto;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	border: 1px solid black;
-	margin: 0 7.5px 15px;
-	overflow: hidden;
+  height: ${({ size }) => (size ? "380px" : "240px")};
+  min-width: 30%;
+  overflow: hidden;
+  flex: 1 1 auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid black;
+  margin: 0 7.5px 15px;
+  overflow: hidden;
 
-	&:hover {
-		cursor: pointer;
+  &:hover {
+    cursor: pointer;
+    & div:first-child {
+      transform: scale(1.1);
+      transition: transform 6s cubic-bezier(0.25, 0.45, 0.45, 0.95);
+    }
+    & div:last-child {
+      opacity: 0.9;
+    }
+  }
 
-		& .background-image {
-			transform: scale(1.1);
-			transition: transform 6s cubic-bezier(0.25, 0.45, 0.45, 0.95);
-		}
-
-		& .content {
-			opacity: 0.9;
-		}
-	}
-
-	&:first-child {
+  &:first-child {
     margin-right: 7.5px;
   }
 
